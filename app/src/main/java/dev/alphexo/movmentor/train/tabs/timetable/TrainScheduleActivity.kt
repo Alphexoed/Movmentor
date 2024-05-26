9ee377f82f2d8547aef7c314e973bf7322a963d7
Alphexo <github@alphexo.dev>
2024-05-26 22:39:44 +0100

    Show warnings on train trip

--- app/src/main/java/dev/alphexo/movmentor/train/tabs/timetable/TrainScheduleActivity.kt
+++ app/src/main/java/dev/alphexo/movmentor/train/tabs/timetable/TrainScheduleActivity.kt
@@ -1,11 +1,14 @@
 package dev.alphexo.movmentor.train.tabs.timetable
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -13,12 +16,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.CheckCircleOutline
 import androidx.compose.material.icons.rounded.HourglassEmpty
+import androidx.compose.material.icons.rounded.LocationOn
+import androidx.compose.material.icons.rounded.PushPin
 import androidx.compose.material.icons.rounded.StarOutline
 import androidx.compose.material.icons.rounded.WarningAmber
 import androidx.compose.material3.Card
@@ -27,6 +31,9 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -34,9 +41,11 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
@@ -56,7 +65,9 @@ import dev.alphexo.movmentor.ui.theme.successContainer
 import dev.alphexo.movmentor.ui.theme.warningContainer
 import dev.alphexo.movmentor.utils.DateFormats
 import dev.alphexo.movmentor.utils.convertDateFormat
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 
@@ -67,6 +78,7 @@ class TrainScheduleActivity : ComponentActivity() {
 
         val currentStation: String = intent.getStringExtra("currentStation") ?: "null"
         val trainNumber: Int = intent.getIntExtra("trainNumber", -1)
+        val warnings: String? = intent.getStringExtra("warnings")
         val date: String = convertDateFormat(
             originalFormat = DateFormats.dayMonthYear,
             desiredFormat = DateFormats.yearMonthDay,
@@ -76,9 +88,23 @@ class TrainScheduleActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MovmentorTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                val coroutineScope = rememberCoroutineScope()
+                val snackbarHostState = remember { SnackbarHostState() }
+
+                Scaffold(
+                    snackbarHost = {
+                        SnackbarHost(hostState = snackbarHostState)
+                    }, modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
                     Surface(modifier = Modifier.padding(innerPadding)) {
-                        ScheduleContent(trainNumber, date, currentStation)
+                        ScheduleContent(
+                            trainNumber,
+                            warnings,
+                            date,
+                            currentStation,
+                            coroutineScope,
+                            snackbarHostState
+                        )
                     }
                 }
             }
@@ -86,25 +112,16 @@ class TrainScheduleActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun titleCardData(index: Int, value: TripSchedule?): List<Any> {
-    val (title: String, data: Pair<String?, Modifier>) = when (index) {
-        0 -> "Destination" to (value?.destinationStationName to Modifier.padding(
-            bottom = 4.dp,
-            end = 4.dp
-        ))
-
-        1 -> "Duration" to (value?.tripDuration to Modifier.padding(start = 4.dp, bottom = 4.dp))
-        2 -> "Departure" to (value?.departureDate to Modifier.padding(top = 4.dp, end = 4.dp))
-        3 -> "Destination" to (value?.destinationDate to Modifier.padding(start = 4.dp, top = 4.dp))
-        else -> throw IllegalArgumentException("Invalid index: $index (expected 0..3)") // Specific exception
-    }
-
-    return listOf(title, data.first ?: stringResource(R.string.please_wait), data.second)
-}
 
 @Composable
-fun ScheduleContent(trainNumber: Int, date: String, currentStation: String) {
+fun ScheduleContent(
+    trainNumber: Int,
+    warnings: String?,
+    date: String,
+    currentStation: String,
+    coroutineScope: CoroutineScope,
+    snackbarHostState: SnackbarHostState
+) {
     val tripEndpoint = Trip()
     var isLoading by remember { mutableStateOf(true) }
     val tripSchedule = remember { mutableStateOf<TripSchedule?>(null) }
@@ -137,16 +154,57 @@ fun ScheduleContent(trainNumber: Int, date: String, currentStation: String) {
 
     Column(modifier = Modifier.padding(16.dp)) {
         // Title Stuff
-        LazyVerticalGrid(
-            columns = GridCells.Fixed(2), modifier = Modifier.fillMaxWidth()
-        ) {
-            items(4) { index ->
-                titleCardData(index, tripSchedule.value).let {
-                    TitleItemCard(
-                        title = it[0] as String,
-                        body = it[1] as String,
-                        modifier = it[2] as Modifier
+        Row {
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1F)
+            ) {
+                // Content of the card
+                Column(Modifier.padding(16.dp)) {
+                    Text(text = "Destination", style = MaterialTheme.typography.titleLarge)
+                    Text(
+                        text = tripSchedule.value?.destinationStationName
+                            ?: stringResource(R.string.please_wait)
                     )
+                }
+            }
+            Card(
+                modifier = Modifier
+                    .wrapContentWidth()
+                    .padding(start = 4.dp)
+            ) {
+                // Content of the card
+                Column(Modifier.padding(16.dp)) {
+                    Text(text = "Duration", style = MaterialTheme.typography.titleLarge)
+                    Text(
+                        text = tripSchedule.value?.tripDuration
+                            ?: stringResource(R.string.please_wait)
+                    )
+                }
+            }
+        }
+
+        AnimatedVisibility(
+            visible = !warnings.isNullOrEmpty(),
+        ) {
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 4.dp),
+                colors = CardDefaults.cardColors(
+                    containerColor = MaterialTheme.colorScheme.warningContainer
+                )
+            ) {
+                Row(Modifier.padding(4.dp), verticalAlignment = Alignment.CenterVertically) {
+                    Icon(
+                        imageVector = Icons.Rounded.WarningAmber,
+                        tint = MaterialTheme.colorScheme.onWarningContainer,
+                        contentDescription = null,
+                        modifier = Modifier.padding(8.dp)
+                    )
+
+                    Text(text = warnings!!)
                 }
             }
         }
@@ -157,7 +215,7 @@ fun ScheduleContent(trainNumber: Int, date: String, currentStation: String) {
         ) {
             tripSchedule.value?.schedule?.forEachIndexed { index, tripStop ->
                 item(index) {
-                    StopItem(tripStop, currentStation)
+                    StopItem(tripStop, currentStation, coroutineScope, snackbarHostState)
                 }
             }
         }
@@ -165,18 +223,12 @@ fun ScheduleContent(trainNumber: Int, date: String, currentStation: String) {
 }
 
 @Composable
-fun TitleItemCard(title: String, body: String, modifier: Modifier) {
-    Card(modifier) {
-        // Content of the card
-        Column(Modifier.padding(16.dp)) {
-            Text(text = title, style = MaterialTheme.typography.titleLarge)
-            Text(text = body)
-        }
-    }
-}
-
-@Composable
-fun StopItem(tripStop: TripStop, currentStation: String) {
+fun StopItem(
+    tripStop: TripStop,
+    currentStation: String,
+    coroutineScope: CoroutineScope,
+    snackbarHostState: SnackbarHostState
+) {
     Column(
         modifier = Modifier.padding(top = 4.dp, bottom = 4.dp)
     ) {
@@ -187,11 +239,21 @@ fun StopItem(tripStop: TripStop, currentStation: String) {
                     tripStop.trainPassed -> EnumStopCard.PASSED
                     else -> EnumStopCard.PENDING
                 }
-                StopCardIcon(type = iconType)
+                StopCardIcon(iconType)
 
                 if (tripStop.warnings.isNotEmpty() || tripStop.warnings === "null") {
                     Spacer(modifier = Modifier.size(4.dp))
-                    StopCardIcon(type = EnumStopCard.WARNING)
+                    Box(Modifier.clickable {
+                        coroutineScope.launch {
+                            snackbarHostState.showSnackbar(
+                                message = tripStop.warnings,
+                                withDismissAction = true,
+                                duration = SnackbarDuration.Long
+                            )
+                        }
+                    }) {
+                        StopCardIcon(EnumStopCard.WARNING)
+                    }
                 }
             }
 
@@ -214,9 +276,11 @@ fun StopItem(tripStop: TripStop, currentStation: String) {
 
 
 @Composable
-fun StopCardIcon(type: EnumStopCard) {
+fun StopCardIcon(
+    type: EnumStopCard
+) {
     val (icon: ImageVector, colors: Pair<Color, Color>) = when (type) {
-        EnumStopCard.CURRENT -> Icons.Rounded.StarOutline to (MaterialTheme.colorScheme.warningContainer to MaterialTheme.colorScheme.onWarningContainer)
+        EnumStopCard.CURRENT -> Icons.Rounded.LocationOn to (MaterialTheme.colorScheme.warningContainer to MaterialTheme.colorScheme.onWarningContainer)
         EnumStopCard.PENDING -> Icons.Rounded.HourglassEmpty to (MaterialTheme.colorScheme.informationContainer to MaterialTheme.colorScheme.onInformationContainer)
         EnumStopCard.PASSED -> Icons.Rounded.CheckCircleOutline to (MaterialTheme.colorScheme.successContainer to MaterialTheme.colorScheme.onSuccessContainer)
         EnumStopCard.WARNING -> Icons.Rounded.WarningAmber to (MaterialTheme.colorScheme.warningContainer to MaterialTheme.colorScheme.onWarningContainer)
